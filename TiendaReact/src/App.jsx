e5e81c5dcbff7b0be951df899327b6f663b72883
Coco-company <guillermohetairoi@gmail.com>
2026-05-24 04:14:23 -0300

    Carro previo a comparacion con ejemplo clase

--- TiendaReact/src/App.jsx
+++ TiendaReact/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer.jsx';
 import ProductoDetalle from './componentes/ProductoDetalle/ProductoDetalle.jsx';
 import Home from './componentes/Home/Home.jsx';
+//*import Carrito from './';
 
 function App() {
 
@@ -16,9 +17,12 @@ function App() {
         <Route path='/Productos' element={<ItemListContainer />} />
         <Route path='/alta' element={<FormularioContainer />} />
         <Route path='/Producto/:id' element={<ProductoDetalle />} />
+        <Route path='/Carrito' element={<ProductoDetalle />} />
       </Route>
     </Routes>
   );
 }
 
 export default App;
+
+
