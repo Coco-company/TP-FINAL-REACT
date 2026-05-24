fa938aa68142a5ec200aeec469828b5af08abb05
Coco-company <guillermohetairoi@gmail.com>
2026-05-24 02:09:05 -0300

    Modificación suave de esteticas

--- TiendaReact/src/componentes/FormularioProducto/FormularioProducto.jsx
+++ TiendaReact/src/componentes/FormularioProducto/FormularioProducto.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import style from './FormularioProducto.module.css';
+import styles from './FormularioProducto.module.css';
 
 export function FormularioProducto({datosForm, manejarCambio, manejarEnvio}){
 
     return (
-        <form className={style.form} onSubmit={manejarEnvio} >
-            <h3 className={style.h3}>Alta de Producto</h3>
-            <div>
-                <label>Producto </label>
-                <input type="text" placeholder="" name="nombre" value={datosForm.nombre} onChange={manejarCambio} />
-                
-                <label>Precio $ </label>
-                <input type="number" placeholder="0" name="precio" value={datosForm.precio} onChange={manejarCambio} />
-                
-                <label>Stock </label>
-                <input type="number" placeholder="Coloque la cantidad en depósito" name="stock" value={datosForm.stock} onChange={manejarCambio} />
-                                
-                <label>Descripcion </label>
-                <textarea placeholder="Coloque la descripcion del producto" name="descripcion" value={datosForm.descripcion} rows="5" onChange={manejarCambio} />
+        <div className={styles.divProductos}>
+            <form className={styles.form} onSubmit={manejarEnvio} >
+                <h3 className={styles.h3}>Alta de Producto</h3>
+                <div>
+                    <label>Producto </label>
+                    <input type="text" placeholder="" name="nombre" value={datosForm.nombre} onChange={manejarCambio} />
+                    
+                    <label>Precio $ </label>
+                    <input type="number" placeholder="0" name="precio" value={datosForm.precio} onChange={manejarCambio} />
+                    
+                    <label>Stock </label>
+                    <input type="number" placeholder="Coloque la cantidad en depósito" name="stock" value={datosForm.stock} onChange={manejarCambio} />
+                                    
+                    <label>Descripcion </label>
+                    <textarea placeholder="Coloque la descripcion del producto" name="descripcion" value={datosForm.descripcion} rows="5" onChange={manejarCambio} />
 
-                <label>Imagen </label>
-                <input type="file" placeholder="https://..." name="imagen" value={datosForm.imagen} onChange={manejarCambio} />
-            </div>
-            
-            <button type="submit">Guardar Producto</button>
-        </form>
+                    <label>Imagen </label>
+                    <input type="file" placeholder="https://..." name="imagen" value={datosForm.imagen} onChange={manejarCambio} />
+                </div>
+                
+                <button type="submit">Guardar Producto</button>
+            </form>
+        </div>
     )
 }
--- TiendaReact/src/componentes/Home/Home.jsx
+++ TiendaReact/src/componentes/Home/Home.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import styles from './Home.module.css';
-import { Link } from 'react-router-dom';
 import OfertasListContainer from './OfertasListContainer/OfertasListContainer.jsx';
 
 function Home() {
