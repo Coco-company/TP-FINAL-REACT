e442c516c25af4159b2f94be486a3fca0babaf3e
Coco-company <guillermohetairoi@gmail.com>
2026-07-18 19:08:33 -0300

    Intento Correccion Boton eliminar

--- TiendaReact/src/componentes/Carrito/Cart.jsx
+++ TiendaReact/src/componentes/Carrito/Cart.jsx
@@ -44,7 +44,7 @@ const Cart = () => {
             <p>Subtotal: ${item.precio * item.quantity}</p>
           </div>
           <button onClick={()=>removeItem(item.id)}> 
-            <img src={"../images/iconoEliminar.svg"} alt={`eliminar ${item.nombre}`} />
+            <img src={"./public/images/iconoEliminar.png"} alt={`eliminar ${item.nombre}`} />
           </button>
         </div>
       ))}
